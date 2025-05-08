7c5f1fc64d6025c39b4136727c7c28af1ce15a6f
Avinash <avin.roy001@gmail.com>
2025-05-08 14:59:29 +0530

    api implementation done

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-amber-800 font-bold underline">
-    Hello world!
-  </h1>
+      <h1>hi</h1>
     </div>
   );
 }
