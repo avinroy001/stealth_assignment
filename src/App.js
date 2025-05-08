377dab0ae0fbfee0c141c5c3d33b7c80d5904b80
Avinash <avin.roy001@gmail.com>
2025-05-08 12:19:53 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-amber-200 font-bold underline">
+      <h1 className="text-3xl text-amber-800 font-bold underline">
     Hello world!
   </h1>
     </div>
